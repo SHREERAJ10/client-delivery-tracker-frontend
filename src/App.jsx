59495347cce2d9315786f849f7ab0055bc5f539f
Shreeraj Shrestha <shreerajshrestha.dev@gmail.com>
2026-03-18 11:17:42 +0545

    chore: setup react router

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,25 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Login from "./pages/Auth/Login.jsx";
+import Dashboard from "./pages/Dashboard.jsx";
+import ProtectedRoute from "./components/ProtectedRoute.jsx";
+
 function App() {
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      Component: ProtectedRoute,
+      children: [
+        { index: true, Component: Dashboard },
+      ],
+    },
+    { path: "/login", Component: Login },
+  ]);
 
   return (
     <>
-      <h1 className="bg-red-500">Hello World</h1>
+    <RouterProvider router={router} />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
