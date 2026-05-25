a3d08d5f1811a6985299607fc9c6e74899cdb7e2
Shreeraj Shrestha <shreerajshrestha.dev@gmail.com>
2026-05-25 16:04:44 +0545

    feat(deliverables): filter deliverables - upcoming or overdue

--- src/pages/DeliverablesPage.jsx
+++ src/pages/DeliverablesPage.jsx
@@ -1,24 +1,35 @@
 import DeliverableOverviewTable from '@/components/DeliverableOverviewTable.jsx'
+import Filter from '@/components/Filter.jsx';
 import SearchBar from '@/components/SearchBar.jsx';
 import AuthContext from '@/context/AuthContext.jsx';
 import { getData } from '@/utils/api.js';
 import React, { useContext, useEffect, useState } from 'react'
-import { useParams, useSearchParams } from 'react-router-dom';
+import { useSearchParams } from 'react-router-dom';
 
 const deliverablesRoute = "/dashboard/deliverables";
+const filterOptions = [
+    {
+        id: crypto.randomUUID(),
+        status: "upcoming"
+    },
+    {
+        id: crypto.randomUUID(),
+        status: "overdue"
+    },
+]
 
 function DeliverablesPage() {
     const { user } = useContext(AuthContext);
     const [deliverables, setDeliverables] = useState([]);
     const [searchParams, setSearchParams] = useSearchParams();
+    const [deliverableType, setDeliverableType] = useState("");
     const currPage = Number(searchParams.get("page") || 1);
     const itemsPerPage = 10;
     const type = searchParams.get('type');
-    console.log(deliverables)   
 
     useEffect(() => {
         (async () => {
-            const data = await getData(user, `/dashboard/deliverables/?page=${currPage}&type=${type}`);
+            const data = await getData(user, `/dashboard/deliverables/?page=${currPage}${type ? `&type=${type}` : ""}`);
             setDeliverables(data);
         })();
     }, [searchParams]);
@@ -29,7 +40,20 @@ function DeliverablesPage() {
 
     return (
         <div>
-            <SearchBar placeholder="Search deliverables by name or status..." currPage={currPage} setSearchResult={setDeliverables} route={deliverablesRoute} />
+            <section className="flex justify-between gap-4 px-7">
+                <SearchBar placeholder="Search deliverables by client, project, deliverable name or status..." currPage={currPage} setSearchResult={setDeliverables} route={deliverablesRoute} />
+                <Filter options={filterOptions} value={deliverableType} onChange={(value) => {
+                    setDeliverableType(value);
+                    if (value != "") {
+                        setSearchParams({ type: value });
+                    }
+                    else {
+                        searchParams.delete("type");
+                        setSearchParams(searchParams);
+                    }
+                }} />
+            </section>
+
             <DeliverableOverviewTable deliverables={deliverables.items} type={type} />
             <div className="flex justify-around">
                 <button
