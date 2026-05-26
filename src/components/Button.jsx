727997ac7e92eac9a6a5d2c934df7e0a44bc4854
Shreeraj Shrestha <shreerajshrestha.dev@gmail.com>
2026-05-26 15:17:25 +0545

    feat: create reusable button component

---
+++ src/components/Button.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Button({ className, onClick = () => { }, type = "", variant = "", children }) {
+    return (
+        <button type={type} className={`flex items-center justify-between gap-2 p-4 py-2 text-sm font-medium transition whitespace-nowrap ${variant == "outline" ? "bg-white border border-black text-[#111]" : "bg-[#111] text-white hover:brightness-110"} ${className}`} onClick={onClick}>
+            {children}
+        </button>
+    )
+}
+
+export default Button
